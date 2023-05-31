cdc8aa6911ed9e6004d7d6e99d80dced7c19925c
Sanderson Nyange <sandersonnyange@gmail.com>
2023-05-31 11:01:08 -0700

    Few changes

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (new == NULL)
 		return (NULL); /* Incase fails to allocate memory*/
 
-	new->a = value;
+	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
